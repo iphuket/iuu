c303232d327864114d8d4c73b237b443edf801f9
aimo-x <aimo-x@oovmi.com>
2019-05-13 18:02:28 +0800

    0513

--- pkt.go
+++ pkt.go
@@ -1,9 +1,10 @@
-// Package iuu it's a web plus tool
-package iuu
+// Package pkt it's a web plus tool
+package pkt
 
 import (
 	"github.com/iphuket/pkt/app"
 )
+
 // Run ...
 func Run(port string) {
 	app.Run(port)
