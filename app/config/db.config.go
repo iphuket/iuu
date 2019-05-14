20e095e3b19d50a5ffe850dc7ba5658200b5dcd8
aimo-x <aimo-x@oovmi.com>
2019-05-14 18:20:11 +0800

    0514-2

--- app/config/db.config.go
+++ app/config/db.config.go
@@ -19,7 +19,7 @@ func DB(drive string) (db *gorm.DB, err error) {
 		db, err := sqlite3.New(sqlite3PATH)
 		return db, err
 	case "mysql":
-		db, err := mysql.New("pkt", "pkt", "pkt", "162.219.121.56:3306")
+		db, err := mysql.New("pkg", "pkg", "pkg", "162.219.121.56:3306")
 		return db, err
 	}
 	return nil, fmt.Errorf("not this drive %s", drive)
